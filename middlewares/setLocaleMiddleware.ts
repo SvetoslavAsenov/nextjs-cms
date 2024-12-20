7881a425f653ddd29e3530b7d11a0c59326936c3
Svetoslav Asenov <svetlio.asenov@gmail.com>
2024-12-20 11:59:25 +0200

    Constants converted to typescript.

--- middlewares/setLocaleMiddleware.ts
+++ middlewares/setLocaleMiddleware.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from "next/server";
 import { defaultLocale, supportedLocales } from "@/constants/server/index";
+import type { SupportedLocale } from "@/constants/server/index";
 
 export default function setLocaleMiddleware(request: Request): NextResponse {
   const LOCALE_SLUG_POSITION = 1;
   const url = new URL(request.url);
   const pathSegments = url.pathname.split("/");
-  const possibleLocaleSlug = pathSegments[LOCALE_SLUG_POSITION];
+  const possibleLocaleSlug = pathSegments[
+    LOCALE_SLUG_POSITION
+  ] as SupportedLocale;
   const isValidLocaleSlug = supportedLocales.includes(possibleLocaleSlug);
   const locale = isValidLocaleSlug ? possibleLocaleSlug : defaultLocale;
   let response;
