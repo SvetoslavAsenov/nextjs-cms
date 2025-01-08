2165d701c43e25707755bc12ae42a003ac43cdfb
Svetoslav Asenov <svetlio.asenov@gmail.com>
2025-01-08 18:04:02 +0200

    Renames of the RegistrationInvite and User model files and classnames.

--- models/User.ts
+++
@@ -1,6 +0,0 @@
-import BaseModel from "./BaseModel";
-import { prisma } from "../lib/prisma";
-
-export default class User extends BaseModel {
-  protected static tableName: keyof typeof prisma = "user";
-}
