4bb9a3c6d657ecb79a328a47265686d3411547e8
Svetoslav Asenov <svetlio.asenov@gmail.com>
2025-01-10 21:58:27 +0200

    Add theme utils

---
+++ utils/theme/theme.server.ts
@@ -0,0 +1,28 @@
+import { getValidThemeAndMode } from "./theme";
+import { getCookieValuesByKeys, setCookies } from "../cookies/cookies.server";
+import { THEME_NAME_COOKIE, THEME_MODE_COOKIE } from "@/constants/cookies";
+import type { SupportedTheme, SupportedMode } from "@/types/theme";
+
+export const setTheme = async (theme: SupportedTheme, mode: SupportedMode) => {
+  await setCookies([
+    [THEME_NAME_COOKIE, theme],
+    [THEME_MODE_COOKIE, mode],
+  ]);
+};
+
+export const getCurrentThemeAndMode = async (): Promise<{
+  theme: SupportedTheme;
+  mode: SupportedMode;
+}> => {
+  const {
+    [THEME_NAME_COOKIE]: themeCookieVal,
+    [THEME_MODE_COOKIE]: modeCookieVal,
+  } = await getCookieValuesByKeys([THEME_NAME_COOKIE, THEME_MODE_COOKIE]);
+
+  const { theme, mode } = getValidThemeAndMode(
+    themeCookieVal ?? "",
+    modeCookieVal ?? ""
+  );
+
+  return { theme, mode };
+};
