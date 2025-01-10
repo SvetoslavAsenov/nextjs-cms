f0491009340b2910b171553548b7857520474f8f
Svetoslav Asenov <svetlio.asenov@gmail.com>
2025-01-10 22:48:42 +0200

    setSthemeAndMode extended to set also the proper classes to the root element

--- utils/theme/theme.client.ts
+++ utils/theme/theme.client.ts
@@ -4,6 +4,7 @@ import {
 } from "@/utils/cookies/cookies.client";
 import { THEME_NAME_COOKIE, THEME_MODE_COOKIE } from "@/constants/cookies";
 import { getValidThemeAndMode } from "./theme";
+import { supportedThemes, defaultMode, supportedModes } from "@/config/themes";
 
 import type { SupportedTheme, SupportedMode } from "@/types/theme";
 
@@ -19,4 +20,18 @@ export const setThemeAndMode = (theme: SupportedTheme, mode: SupportedMode) => {
     [THEME_NAME_COOKIE, theme],
     [THEME_MODE_COOKIE, mode],
   ]);
+
+  // Set the theme and mode classes on the root HTML element
+  const root = document.documentElement;
+
+  // Remove previous theme and mode classes if any
+  root.classList.remove(...supportedThemes, ...supportedModes);
+
+  // Add the new theme and mode classes
+  root.classList.add(theme);
+
+  // Handle mode class
+  if (mode !== defaultMode) {
+    root.classList.add(mode);
+  }
 };
