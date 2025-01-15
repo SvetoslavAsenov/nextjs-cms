d348672be62c1bdefe030a24046193c7042995a6
Svetoslav Asenov <svetlio.asenov@gmail.com>
2025-01-15 19:12:20 +0200

    Install shadcn label and input. Remove unwanted providers from auth.config.

--- config/auth/authProviders.ts
+++ config/auth/authProviders.ts
@@ -1,8 +1,5 @@
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
-import Facebook from "next-auth/providers/facebook";
-import Instagram from "next-auth/providers/instagram";
-import Twitter from "next-auth/providers/twitter";
 import Linkedin from "next-auth/providers/linkedin";
 
 import type { Provider } from "next-auth/providers";
@@ -11,7 +8,10 @@ const CREDENTIALS_PROVIDER_NAME = "credentials";
 
 export const providersConfig: Provider[] = [
   Credentials({
-    credentials: { password: { label: "Password", type: "password" } },
+    credentials: {
+      username: { label: "Username", type: "text" },
+      password: { label: "Password", type: "password" },
+    },
     authorize(c) {
       if (c.password !== "password") return null;
       return {
@@ -22,9 +22,6 @@ export const providersConfig: Provider[] = [
     },
   }),
   Google,
-  Facebook,
-  Instagram,
-  Twitter,
   Linkedin,
 ];
 
