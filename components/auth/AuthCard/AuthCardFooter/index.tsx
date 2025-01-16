c44da2f9400e400f5b6926bf91cd297ca5453abf
Svetoslav Asenov <svetlio.asenov@gmail.com>
2025-01-16 13:15:03 +0200

    Render oauth provider buttons from predefined constant.

--- components/auth/AuthCard/AuthCardFooter/index.tsx
+++ components/auth/AuthCard/AuthCardFooter/index.tsx
@@ -1,12 +1,22 @@
+"use client";
+
+import { signIn } from "next-auth/react";
 import { CardFooter } from "@/components/shadcn/ui/card";
 import { Button } from "@/components/shadcn/ui/button";
-import GoogleIcon from "@/assets/icons/google-icon-logo.svg";
-import LinkedinLogo from "@/assets/icons/linkedin-icon-logo.svg";
 import Image from "next/image";
+import { OAUTH_PROVIDERS } from "@/constants/oauthProviders";
 
 import type { AuthCardFooterProps } from "../AuthCard.types";
 
 export const AuthCardFooter = ({ translations }: AuthCardFooterProps) => {
+  const btnHandler = (ev: React.MouseEvent<HTMLButtonElement>) => {
+    const target = ev.target as HTMLButtonElement;
+    const provider = target.getAttribute("data-provider");
+    if (provider) {
+      signIn(provider);
+    }
+  };
+
   return (
     <CardFooter className="flex flex-col">
       <div className="relative w-full flex flex-col justify-center items-center mb-6">
@@ -16,22 +26,20 @@ export const AuthCardFooter = ({ translations }: AuthCardFooterProps) => {
         </p>
       </div>
       <div className="flex flex-col w-full gap-4">
-        <Button
-          data-provider="google"
-          variant="outline"
-          className="text-base h-12"
-        >
-          <Image src={GoogleIcon} alt="google" className="w-4" />
-          {translations.providerButtonLabel} {translations.google}
-        </Button>
-        <Button
-          data-provider="linkedin"
-          variant="outline"
-          className="text-base h-12"
-        >
-          <Image src={LinkedinLogo} alt="linkedin" className="w-4" />
-          {translations.providerButtonLabel} {translations.linkedin}
-        </Button>
+        {OAUTH_PROVIDERS.map((provider) => {
+          return (
+            <Button
+              data-provider={provider.name}
+              variant="outline"
+              className="text-base h-12"
+              onClick={btnHandler}
+              key={provider.name}
+            >
+              <Image src={provider.icon} alt={provider.name} className="w-4" />
+              {translations.providerButtonLabel} {translations.google}
+            </Button>
+          );
+        })}
       </div>
     </CardFooter>
   );
---
+++ constants/oauthProviders.ts
@@ -0,0 +1,13 @@
+import GoogleLogoIcon from "@/assets/icons/google-icon-logo.svg";
+import LinkedinLogoIcon from "@/assets/icons/linkedin-icon-logo.svg";
+
+export const OAUTH_PROVIDERS = [
+  {
+    name: "google",
+    icon: GoogleLogoIcon,
+  },
+  {
+    name: "linkedin",
+    icon: LinkedinLogoIcon,
+  },
+];
