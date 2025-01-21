8ee754e4939956f83b7aa4ae118543ef94659a20
Svetoslav Asenov <svetlio.asenov@gmail.com>
2025-01-21 04:46:55 +0200

    Create app layout

---
+++ app/[locale]/(panel)/layout.tsx
@@ -0,0 +1,17 @@
+import AppHeader from "@/components/AppHeader";
+import { SupportedLocale } from "@/types/locales";
+
+type AppLayoutProps = {
+  children: React.ReactNode;
+  params: { locale: SupportedLocale };
+};
+
+export default async function AppLayout({ children, params }: AppLayoutProps) {
+  const { locale } = await params;
+  return (
+    <div className="app-wrapper">
+      <AppHeader locale={locale} />
+      <main>{children}</main>
+    </div>
+  );
+}
