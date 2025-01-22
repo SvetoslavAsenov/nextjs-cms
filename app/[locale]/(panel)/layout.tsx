a1720b77e0e81ffe6df0b3eaac528f01c0bc9d32
Svetoslav Asenov <svetlio.asenov@gmail.com>
2025-01-22 14:18:57 +0200

    Move all providers to main layout

--- app/[locale]/(panel)/layout.tsx
+++ app/[locale]/(panel)/layout.tsx
@@ -1,10 +1,5 @@
 import AppHeader from "@/components/AppHeader";
 import { SupportedLocale } from "@/types/locales";
-import LocaleProvider from "@/components/providers/LocaleProvider";
-import TranslateProvider from "@/components/providers/TranslateProvider";
-import AuthProvider from "@/components/providers/AuthProvider";
-import { auth } from "@/lib/auth";
-import type { User } from "@prisma/client";
 
 type AppLayoutProps = {
   children: React.ReactNode;
@@ -12,25 +7,12 @@ type AppLayoutProps = {
 };
 
 export default async function AppLayout({ children, params }: AppLayoutProps) {
-  let user;
   const { locale } = await params;
-  const authResult = await auth();
-
-  if (authResult?.user?.email) {
-    const { id, email, name, image, roleId } = authResult?.user as User;
-    user = { id, email, name, image, roleId };
-  }
 
   return (
-    <LocaleProvider locale={locale}>
-      <TranslateProvider>
-        <AuthProvider user={user}>
-          <div className="app-wrapper">
-            <AppHeader locale={locale} />
-            <main>{children}</main>
-          </div>
-        </AuthProvider>
-      </TranslateProvider>
-    </LocaleProvider>
+    <div className="app-wrapper">
+      <AppHeader locale={locale} />
+      <main>{children}</main>
+    </div>
   );
 }
