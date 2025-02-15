6df268f4c12a9a817aa652a74c3709a5cdeb6c0b
Svetoslav Asenov <svetlio.asenov@gmail.com>
2025-02-16 01:17:53 +0200

    Add methods to RoleModel for adding or checking role permissions.

--- models/RoleModel.ts
+++ models/RoleModel.ts
@@ -2,6 +2,8 @@ import { Prisma, Role } from "@prisma/client";
 import BaseModel from "./BaseModel";
 import { prisma } from "@/lib/prisma";
 
+import type { Permission } from "@/config/authorization/permissions";
+
 export default class RoleModel extends BaseModel<
   Prisma.RoleCreateArgs,
   Prisma.RoleDeleteArgs,
@@ -14,4 +16,50 @@ export default class RoleModel extends BaseModel<
   constructor() {
     super(prisma.role);
   }
+
+  public getRolePermissionsByRole = (role: Role) => {
+    if (!Array.isArray(role?.permissions)) {
+      return [];
+    }
+
+    return role.permissions as Permission[];
+  };
+
+  public getRolePermissionsById = async (roleId: string) => {
+    const role = await this.findUnique({
+      where: { id: roleId },
+      select: { permissions: true },
+    });
+
+    return role ? this.getRolePermissionsByRole(role) : [];
+  };
+
+  public addPermissions = async (
+    roleId: string,
+    newPermissions: Permission[]
+  ) => {
+    const role = await this.findUnique({
+      where: { id: roleId },
+    });
+
+    if (!role) {
+      return;
+    }
+
+    const existingPermissions = this.getRolePermissionsByRole(role);
+    const updatedPermissions = Array.from(
+      new Set([...existingPermissions, ...newPermissions])
+    );
+
+    return await this.update({
+      where: { id: roleId },
+      data: { permissions: updatedPermissions },
+    });
+  };
+
+  public hasPermission = async (roleId: string, permission: Permission) => {
+    const existingPermissions = await this.getRolePermissionsById(roleId);
+
+    return existingPermissions.includes(permission);
+  };
 }
