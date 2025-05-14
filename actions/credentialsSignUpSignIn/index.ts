60682aeac2d80d48f310ede93117b5937adc15ab
Svetoslav Asenov <svetlio.asenov@gmail.com>
2025-05-14 11:41:21 +0300

    Fix register bug

--- actions/credentialsSignUpSignIn/index.ts
+++ actions/credentialsSignUpSignIn/index.ts
@@ -5,7 +5,7 @@ import {
 } from "@/schemas/auth";
 import { redirect } from "next/navigation";
 import credentialsAuthService from "./credentialsAuthService";
-import { HOME_URL } from "@/constants/urls";
+import { LOGIN_URL } from "@/constants/urls";
 
 import type {
   CredentialsFormHandlerType,
@@ -14,7 +14,7 @@ import type {
   ResultObjectItemType,
 } from "@/types/actions/CredentialsFormHandlerTypes";
 
-const REDIRECT_URL = HOME_URL;
+const REDIRECT_URL = LOGIN_URL;
 
 const formDataToResultObject: formDataToResultObject = (formData) => {
   const fieldsObject = {
@@ -49,11 +49,9 @@ const CredentialsFormHandler: CredentialsFormHandlerType = async (
     for (const issue of validationResult.error.issues) {
       // If there is an error with the token
       // we will redirect the user.
-      // if (issue?.path?.[0] === "token") {
-      //   resultObject.redirectUrl = REDIRECT_URL;
-      //   return resultObject;
-      // }
-      redirect(REDIRECT_URL);
+      if (issue?.path?.[0] === "token") {
+        redirect(REDIRECT_URL);
+      }
       const errorCode: string | undefined = issue?.code;
       const errorMsg: string | undefined = issue?.message;
       resultObject.errors[issue?.path?.[0]] =
